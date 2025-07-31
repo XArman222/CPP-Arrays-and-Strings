456b4dc10149a4f4e3b9d52b66d4388247078f02
XArman222 <xsoulop@gmail.com>
2025-07-31 00:37:41 -0400

    Update Concat.cpp

--- Concat.cpp
+++ Concat.cpp
@@ -1,1 +1,17 @@
+//Arman Majhi
+//24070123022
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str1 = "Hello";
+    string str2 = "World";
+
+    string result = str1 + " " + str2;
+  
+    cout << "Concatenated string: " << result << endl;
+
+    return 0;
+}
